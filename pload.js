91daa58ab08fae3c4df91026dd6551d2781af624
c0ff33b34n <86168298+c0ff33b34n@users.noreply.github.com>
2022-07-26 22:55:36 +0530

    Delete pload.js

--- pload.js
+++
@@ -1,7 +0,0 @@
-const fadeOut = () => {
-  const loaderWrapper.classList.add('fade');
-  document.querySelector('.wrapper');
-  loaderWrapper.classList.add('fade');
-}
-
-window.addEventListener('load', fadeOut);
